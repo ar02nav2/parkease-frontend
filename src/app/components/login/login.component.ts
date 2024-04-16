2bdf0753c39d90c118297969f805a863277bc188
ar02nav2 <vishuupadhyay79@gmail.com>
2024-04-16 13:02:26 +0530

    things updated

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,36 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { SignupComponent } from '../signup/signup.component';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../../auth.service';
 
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive,SignupComponent,ReactiveFormsModule,CommonModule],
+  imports: [RouterLink, RouterLinkActive,ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit {
-  loginForm = new FormGroup({
-    email: new FormControl("", [Validators.required, Validators.email]),
-    password: new FormControl("", [Validators.required,Validators.maxLength(25)]),
+export class LoginComponent {
+  fb = inject(FormBuilder);
+  http = inject(HttpClient);
+  authservice = inject(AuthService);
+  router = inject(Router);
+
+  form = this.fb.nonNullable.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
   });
-
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  get email() {
-    return this.loginForm.get('email')!;
+  errorMessage: string | null = null;
+email: any;
+password: any;
+  
+  onSubmit(): void {
+    const rawForm = this.form.getRawValue();
+    this.authservice
+      .login(rawForm.email, rawForm.password)
+      .subscribe({
+        next : () => {
+    this.router.navigateByUrl('/');
+  },
+  error: (err) => {
+    this.errorMessage = err.code;
   }
-
-  get password() {
-    return this.loginForm.get('password')!;
-  }
-
+  });
 }
+}
+
 
 
